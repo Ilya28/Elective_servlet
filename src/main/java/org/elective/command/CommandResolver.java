c4152dc305e9357ef4eda40aff7a2d37754c0c94
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 15:41:31 +0200

    Basic implementation

--- src/main/java/org/elective/command/CommandResolver.java
+++ src/main/java/org/elective/command/CommandResolver.java
@@ -1,9 +1,28 @@
 package org.elective.command;
 
+import org.elective.command.commands.NotExistCommand;
+import org.elective.command.tools.CommandKey;
+import org.elective.command.tools.CommandsMap;
 import org.elective.command.tools.RequestMethod;
+import static org.elective.command.tools.RequestMethod.*;
 
 public class CommandResolver {
+    private static final CommandKey COMMAND_DOES_NOT_EXIST =
+            new CommandKey("Not exist", METHOD_ANY);
+
+    private static final CommandsMap commands = new CommandsMap();
+    static {
+        commands.addMapping(COMMAND_DOES_NOT_EXIST, new NotExistCommand());
+    }
+
     public static Command get(String commandName, RequestMethod requestMethod) {
-        return null;
+        return commands.getCommandOrDefault(
+                commandName,
+                requestMethod,
+                commands.getCommand(COMMAND_DOES_NOT_EXIST)
+        );
+    }
+
+    private CommandResolver() {
     }
 }
