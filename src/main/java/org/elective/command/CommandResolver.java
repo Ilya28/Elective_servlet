35be62f620f649e04395d2a0109ed4b6dabfc9d6
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 13:00:13 +0200

    added

---
+++ src/main/java/org/elective/command/CommandResolver.java
@@ -0,0 +1,9 @@
+package org.elective.command;
+
+import org.elective.command.tools.RequestMethod;
+
+public class CommandResolver {
+    public static Command get(String commandName, RequestMethod requestMethod) {
+        return null;
+    }
+}
---
+++ src/main/java/org/elective/command/tools/RequestMethod.java
@@ -0,0 +1,5 @@
+package org.elective.command.tools;
+
+public enum RequestMethod {
+    METHOD_GET, METHOD_POST;
+}
