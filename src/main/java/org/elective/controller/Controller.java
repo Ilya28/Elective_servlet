3e388b456383715dead719a262e7646ca4abca03
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 13:00:57 +0200

    processRequest method added, some refactor

--- src/main/java/org/elective/controller/Controller.java
+++ src/main/java/org/elective/controller/Controller.java
@@ -1,5 +1,9 @@
 package org.elective.controller;
 
+import org.elective.command.Command;
+import org.elective.command.CommandResolver;
+import org.elective.command.tools.RequestMethod;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +13,12 @@ import java.io.IOException;
 
 @WebServlet(name = "Controller")
 public class Controller extends HttpServlet {
+    private final int PATH_PREFIX = 31;
+
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("service()");
-        super.service(req, resp);
+        super.service(request, response);
     }
 
     @Override
@@ -24,12 +30,21 @@ public class Controller extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("doPost()");
-        doGet(request, response);
+        processRequest(request, response, RequestMethod.METHOD_POST);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("doGet()");
-        request.getRequestDispatcher("test.jsp").forward(request, response);
+        processRequest(request, response, RequestMethod.METHOD_GET);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response, RequestMethod requestMethod) {
+        String path = request.getRequestURI().substring(PATH_PREFIX);
+        System.out.println("Path = " + path);
+
+        Command command = CommandResolver.get(path, requestMethod);
+
+        command.execute(request, response);
     }
 }
