2ccc0e8edd1dd85b75b7a2a32275d11d4d89605d
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-10-30 15:42:23 +0200

    Created

---
+++ src/main/java/org/elective/command/commands/NotExistCommand.java
@@ -0,0 +1,15 @@
+package org.elective.command.commands;
+
+import org.elective.command.Command;
+import org.elective.service.WebPaths;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NotExistCommand implements Command {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        request.setAttribute("message", "404, command (page) does not fond");
+        return WebPaths.convertNameToPath("errorPage");
+    }
+}
