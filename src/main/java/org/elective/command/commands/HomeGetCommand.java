20e073bffc12824caf32965f7af43c8a7a115c10
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-01 18:41:40 +0200

    Registrations list at home page

--- src/main/java/org/elective/command/commands/HomeGetCommand.java
+++ src/main/java/org/elective/command/commands/HomeGetCommand.java
@@ -2,12 +2,16 @@ package org.elective.command.commands;
 
 import org.apache.log4j.Logger;
 import org.elective.command.Command;
+import org.elective.entities.Registration;
 import org.elective.service.RegistrationService;
 import org.elective.service.Security;
 import org.elective.service.WebPaths;
+import org.elective.service.pagination.Page;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.List;
 
 public class HomeGetCommand implements Command {
     private static final Logger log = Logger.getLogger(HomeGetCommand.class);
@@ -22,6 +26,14 @@ public class HomeGetCommand implements Command {
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
+        String username = (String) request.getServletContext().getAttribute(Security.ATTRIBUTE_USERNAME_HOLDER);
+        Page page = registrationService.pagination(request);
+        List<Registration> registrations = Collections.emptyList();
+        if (username != null)
+            registrations = registrationService.getRegistrationsByUserEmail(username, page);
+        else
+            log.error("Null username");
+        request.setAttribute("registrations", registrations);
         Security role = Security.getUserRole(request);
         return WebPaths.nameToPath((role == Security.USER)? USER_HOME: TEACHER_HOME);
     }
