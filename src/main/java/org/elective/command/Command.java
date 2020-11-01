c7723f485cc948aede96301b4c3789a65f8048e2
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-01 17:48:53 +0200

    meow

--- src/main/java/org/elective/command/Command.java
+++ src/main/java/org/elective/command/Command.java
@@ -1,8 +1,10 @@
 package org.elective.command;
 
+import org.elective.dbtools.exceptions.ORMException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Command {
-    String execute(HttpServletRequest request, HttpServletResponse response);
+    String execute(HttpServletRequest request, HttpServletResponse response) throws ORMException;
 }
