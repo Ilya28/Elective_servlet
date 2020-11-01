3c2d0893f999e419e158289ccdd239146598b35f
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-01 17:58:37 +0200

    impl

--- src/main/java/org/elective/command/commands/UserGetCommand.java
+++ src/main/java/org/elective/command/commands/UserGetCommand.java
@@ -1,13 +1,24 @@
 package org.elective.command.commands;
 
 import org.elective.command.Command;
+import org.elective.dbtools.Importer;
+import org.elective.dbtools.exceptions.ORMException;
+import org.elective.entities.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Optional;
 
 public class UserGetCommand implements Command {
+    private static final String PAGE_NAME = "user";
+
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws ORMException {
+        // TODO: condition (for pagination)
+        Importer<User> importer = new Importer<>(null);
+        Optional<User> user = importer.loadOne(User.class, null);
+        request.setAttribute("user", user);
+        return PAGE_NAME;
     }
 }
--- src/main/java/org/elective/command/commands/UsersGetCommand.java
+++ src/main/java/org/elective/command/commands/UsersGetCommand.java
@@ -1,13 +1,23 @@
 package org.elective.command.commands;
 
 import org.elective.command.Command;
+import org.elective.dbtools.Importer;
+import org.elective.dbtools.exceptions.ORMException;
+import org.elective.entities.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class UsersGetCommand implements Command {
+    private static final String PAGE_NAME = "users";
+
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws ORMException {
+        // TODO: condition (for pagination)
+        Importer<User> importer = new Importer<>(null);
+        List<User> users = importer.load(User.class, null);
+        request.setAttribute("users", users);
+        return PAGE_NAME;
     }
 }
