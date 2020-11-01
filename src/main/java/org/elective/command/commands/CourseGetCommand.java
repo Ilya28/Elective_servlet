9279ab3b18e1f77808646458349744a21b51bdeb
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-01 17:53:05 +0200

    execute impl and courseService dependency

--- src/main/java/org/elective/command/commands/CourseGetCommand.java
+++ src/main/java/org/elective/command/commands/CourseGetCommand.java
@@ -1,13 +1,33 @@
 package org.elective.command.commands;
 
 import org.elective.command.Command;
+import org.elective.entities.Course;
+import org.elective.service.CourseService;
+import org.elective.service.WebPaths;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 public class CourseGetCommand implements Command {
+    private final CourseService courseService;
+
+    public CourseGetCommand(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+        int id;
+        try {
+            id = Integer.parseInt(request.getParameter("id"));
+        } catch (NumberFormatException e) {
+            return "redirect:/courses?course_not_fond";
+        }
+        Optional<Course> course = courseService.getCourseById((long) id);
+        if (!course.isPresent())
+            return "redirect:/courses?course_not_fond";
+        request.setAttribute("course", course.get());
+        return WebPaths.nameToPath("course");
     }
 }
--- src/main/java/org/elective/command/commands/CoursesGetCommand.java
+++ src/main/java/org/elective/command/commands/CoursesGetCommand.java
@@ -1,13 +1,31 @@
 package org.elective.command.commands;
 
+import org.apache.log4j.Logger;
 import org.elective.command.Command;
+import org.elective.entities.Course;
+import org.elective.service.CourseService;
+import org.elective.service.pagination.Page;
+import org.elective.service.WebPaths;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class CoursesGetCommand implements Command {
+    private static final String COURSES_PAGE = "courses";
+    private static final Logger log = Logger.getLogger(CoursesGetCommand.class);
+    private final CourseService courseService;
+
+    public CoursesGetCommand(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+        Page page = courseService.pagination(request);
+        List<Course> courses = courseService.getCoursesPage(page);
+        request.setAttribute("pages", page.getCount());
+        request.setAttribute("courses", courses);
+        return WebPaths.nameToPath(COURSES_PAGE);
     }
 }
