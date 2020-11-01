2f8a5ed45bcf5796235a461d68b015fc29b5c337
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-01 17:52:12 +0200

    execute impl

--- src/main/java/org/elective/command/commands/CourseAddPostCommand.java
+++ src/main/java/org/elective/command/commands/CourseAddPostCommand.java
@@ -1,13 +1,34 @@
 package org.elective.command.commands;
 
 import org.elective.command.Command;
+import org.elective.entities.Course;
+import org.elective.service.CourseService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
 
 public class CourseAddPostCommand implements Command {
+    private final CourseService courseService;
+
+    public CourseAddPostCommand(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+        Course course = Course.toBuilder()
+                .nameEN(request.getParameter("name_en"))
+                .nameUA(request.getParameter("name_ua"))
+                .subject(Long.valueOf(request.getParameter("subject")))
+                .descriptionEN(request.getParameter("description_en"))
+                .descriptionUA(request.getParameter("description_ua"))
+                .dateStart(LocalDate.parse(request.getParameter("date_start")))
+                .dateEnd(LocalDate.parse(request.getParameter("date_end")))
+                .seats(Integer.parseInt(request.getParameter("seats")))
+                .signedUp(0)
+                .build();
+        courseService.addCourse(course);
+        return "redirect:/courses";
     }
 }
