3186f7ddb61dc30ffe23de7c3ecf49f8b7825a17
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-01 17:52:40 +0200

    execute impl and courseService dependency

--- src/main/java/org/elective/command/commands/CourseDeleteGetCommand.java
+++ src/main/java/org/elective/command/commands/CourseDeleteGetCommand.java
@@ -1,13 +1,21 @@
 package org.elective.command.commands;
 
 import org.elective.command.Command;
+import org.elective.service.CourseService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CourseDeleteGetCommand implements Command {
+    private final CourseService courseService;
+
+    public CourseDeleteGetCommand(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+        courseService.deleteCourseById(Integer.parseInt(request.getParameter("id")));
+        return "redirect:/courses";
     }
 }
