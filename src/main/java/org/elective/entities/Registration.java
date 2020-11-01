6c392b422c326cd4c9b33256aa6ada6dd19a901d
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-01 17:56:56 +0200

    int to Long and builder pattern impl

--- src/main/java/org/elective/entities/Registration.java
+++ src/main/java/org/elective/entities/Registration.java
@@ -2,29 +2,38 @@ package org.elective.entities;
 
 import org.elective.dbtools.annotations.Table;
 
-@Table(name = "grade_log", fieldsAutoNaming = true)
+@Table(name = "registrations", fieldsAutoNaming = true)
 public class Registration {
-    private int course;     // PK FK
-    private int user;       // PK FK
+    private Long id;
+    private Long course;
+    private Long user;
     private int progress;
     private int grade;
 
     public Registration() {
     }
 
-    public int getCourse() {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCourse() {
         return course;
     }
 
-    public void setCourse(int course) {
+    public void setCourse(Long course) {
         this.course = course;
     }
 
-    public int getUser() {
+    public Long getUser() {
         return user;
     }
 
-    public void setUser(int user) {
+    public void setUser(Long user) {
         this.user = user;
     }
 
@@ -42,5 +51,38 @@ public class Registration {
 
     public void setGrade(int grade) {
         this.grade = grade;
+    }
+
+    public static Builder toBuilder() {
+        return new Builder();
+    }
+    public static class Builder {
+        private final Registration newRegistration;
+        public Builder() {
+            newRegistration = new Registration();
+        }
+        public Builder id(Long newId) {
+            newRegistration.id = newId;
+            return this;
+        }
+        public Builder course(Long newCourse) {
+            newRegistration.course = newCourse;
+            return this;
+        }
+        public Builder user(Long newUser) {
+            newRegistration.user = newUser;
+            return this;
+        }
+        public Builder progress(int newProgress) {
+            newRegistration.progress = newProgress;
+            return this;
+        }
+        public Builder grade(int newGrade) {
+            newRegistration.grade = newGrade;
+            return this;
+        }
+        public Registration build(){
+            return newRegistration;
+        }
     }
 }
