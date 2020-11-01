d10f16f39e77006fe08ab164c7df1d9c4cf9e95d
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-01 17:57:46 +0200

    getAllMatchingFields, setObjectFieldsFromResultSet methods added

--- src/main/java/org/elective/dbtools/Tools.java
+++ src/main/java/org/elective/dbtools/Tools.java
@@ -2,7 +2,15 @@ package org.elective.dbtools;
 
 import org.elective.dbtools.annotations.Table;
 import org.elective.dbtools.annotations.TableField;
+import org.elective.dbtools.exceptions.ORMException;
+
 import java.lang.reflect.Field;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Tools {
     public static String getSQLNameFromField(Field field) {
@@ -19,5 +27,36 @@ public class Tools {
             return clazz.getAnnotation(Table.class).name();
         else
             return NamesConverter.toSQLName(clazz.getName());
+    }
+
+    public static List<Field> getAllMatchingFields(Class<?> clazz) throws ORMException{
+        if (!clazz.isAnnotationPresent(Table.class))
+            throw new ORMException("The class must marked with annotation `Table(...)`");
+        List<Field> fields;
+        if (clazz.getAnnotation(Table.class).fieldsAutoNaming())
+            fields = Arrays.asList(clazz.getDeclaredFields());
+        else
+            fields = Arrays.stream(clazz.getDeclaredFields())
+                    .filter(f->f.isAnnotationPresent(TableField.class))
+                    .collect(Collectors.toList());
+        return fields;
+    }
+
+    public static void setObjectFieldsFromResultSet(Object obj, List<Field> fields, ResultSet rs)
+            throws ORMException {
+        for (Field field : fields) {
+            field.setAccessible(true);
+            String fieldName = Tools.getSQLNameFromField(field);
+            try {
+                Object extracted = rs.getObject(fieldName);
+                if (extracted instanceof java.sql.Date)
+                    extracted = ((java.sql.Date)extracted).toLocalDate();
+                field.set(obj, extracted);
+            } catch (IllegalAccessException exception) {
+                throw new ORMException("Reflection: Illegal access to field `" + field.getName() + "`");
+            } catch (SQLException exception) {
+                throw new ORMException("Unknown field `" + fieldName + "`");
+            }
+        }
     }
 }
