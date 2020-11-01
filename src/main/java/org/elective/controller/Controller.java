fcca16d4b8a826b318421d2d8595f53a7b30e3ed
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-01 17:50:41 +0200

    processRequest method impl

--- src/main/java/org/elective/controller/Controller.java
+++ src/main/java/org/elective/controller/Controller.java
@@ -1,8 +1,11 @@
 package org.elective.controller;
 
+import org.apache.log4j.Logger;
 import org.elective.command.Command;
 import org.elective.command.CommandResolver;
+import org.elective.command.tools.CommandKey;
 import org.elective.command.tools.RequestMethod;
+import org.elective.dbtools.exceptions.ORMException;
 import org.elective.service.WebPaths;
 
 import javax.servlet.ServletException;
@@ -14,43 +17,48 @@ import java.io.IOException;
 
 @WebServlet(name = "Controller")
 public class Controller extends HttpServlet {
+    private static final Logger log = Logger.getLogger(Controller.class);
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("service()");
         super.service(request, response);
     }
 
     @Override
     public void init() throws ServletException {
-        System.out.println("init()");
+        log.info("---===<< Servlete init >>===---");
         super.init();
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("doPost()");
         processRequest(request, response, RequestMethod.METHOD_POST);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("doGet()");
         processRequest(request, response, RequestMethod.METHOD_GET);
     }
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response, RequestMethod requestMethod)
             throws IOException, ServletException {
+        log.info("\n======================================");
         String path = WebPaths.extractFinalPath(request.getRequestURI());
-        System.out.println("Path = " + path);
-
-        Command command = CommandResolver.get(path, requestMethod);
-
-        String forward;
-        forward = command.execute(request, response);
-
-        if (WebPaths.isRedirect(forward))
+        log.info("Path = " + path);
+        Command command = CommandResolver.getCommand(new CommandKey(path, requestMethod));
+        log.info("Command: " + command.getClass().getName());
+        String forward = "error";
+        try {
+            forward = command.execute(request, response);
+        } catch (ORMException e) {
+            e.printStackTrace();
+        }
+        if (WebPaths.isRedirect(forward)) {
+            log.info("Redirect to page: '" + forward + "'");
             response.sendRedirect(WebPaths.getUrlFromRedirection(forward));
-        else
+        } else {
+            log.info("Load page: '" + forward + "'");
             request.getRequestDispatcher(forward).forward(request, response);
+        }
     }
 }
