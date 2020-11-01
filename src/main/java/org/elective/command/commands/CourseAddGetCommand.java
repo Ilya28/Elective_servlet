61f0b5e9a7ac25dc552dd7d3abbfc15504f9e308
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-01 17:51:51 +0200

    mapping

--- src/main/java/org/elective/command/commands/CourseAddGetCommand.java
+++ src/main/java/org/elective/command/commands/CourseAddGetCommand.java
@@ -1,6 +1,7 @@
 package org.elective.command.commands;
 
 import org.elective.command.Command;
+import org.elective.service.WebPaths;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 public class CourseAddGetCommand implements Command {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+        return WebPaths.nameToPath("course_add");
     }
 }
