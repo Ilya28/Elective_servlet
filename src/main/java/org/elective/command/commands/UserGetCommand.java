5c4f8b2b0be5d6648e2b23caea100a9a55d64400
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-02 02:45:19 +0200

    impl

--- src/main/java/org/elective/command/commands/UserGetCommand.java
+++ src/main/java/org/elective/command/commands/UserGetCommand.java
@@ -1,9 +1,13 @@
 package org.elective.command.commands;
 
+import org.apache.log4j.Logger;
 import org.elective.command.Command;
 import org.elective.dbtools.Importer;
 import org.elective.dbtools.exceptions.ORMException;
 import org.elective.entities.User;
+import org.elective.service.RegistrationService;
+import org.elective.service.UserService;
+import org.elective.service.WebPaths;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,13 +16,29 @@ import java.util.Optional;
 
 public class UserGetCommand implements Command {
     private static final String PAGE_NAME = "user";
+    private static final Logger log = Logger.getLogger(UserGetCommand.class);
+
+    UserService userService;
+
+    public UserGetCommand(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws ORMException {
-        // TODO: condition (for pagination)
-        Importer<User> importer = new Importer<>(null);
-        Optional<User> user = importer.loadOne(User.class, null);
-        request.setAttribute("user", user);
-        return PAGE_NAME;
+        Long id;
+        try {
+            id = Long.valueOf(request.getParameter("id"));
+        } catch (NumberFormatException e) {
+            log.error("Wrong id param");
+            return "redirect:/users";
+        }
+        Optional<User> user = userService.getUserById(id);
+        if (!user.isPresent()) {
+            log.error("User not fond");
+            return "redirect:/users";
+        }
+        request.setAttribute("user", user.get());
+        return WebPaths.nameToPath(PAGE_NAME);
     }
 }
