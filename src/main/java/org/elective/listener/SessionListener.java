5515e2bacdf97dcba13933364ba17e592bfd9fe7
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-02 02:43:27 +0200

    logged users in getServletContext, locale in session nd logging

--- src/main/java/org/elective/listener/SessionListener.java
+++ src/main/java/org/elective/listener/SessionListener.java
@@ -1,5 +1,7 @@
 package org.elective.listener;
 
+import org.apache.log4j.Logger;
+import org.elective.command.commands.LogOutCommand;
 import org.elective.service.Security;
 
 import javax.servlet.http.HttpSessionEvent;
@@ -7,20 +9,29 @@ import javax.servlet.http.HttpSessionListener;
 import java.util.HashSet;
 
 public class SessionListener implements HttpSessionListener {
+    private static final Logger log = Logger.getLogger(SessionListener.class);
+
     @Override
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         HashSet<String> loggedUsers = new HashSet<>();
         httpSessionEvent.getSession().setAttribute(Security.ATTRIBUTE_LOGGED_USERS_HOLDER, loggedUsers);
+        httpSessionEvent.getSession().setAttribute("locale", "en");
     }
 
     @Override
     public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
         HashSet<String> loggedUsers = (HashSet<String>) httpSessionEvent
-                .getSession().getServletContext()
+                .getSession()
+                .getServletContext()
                 .getAttribute(Security.ATTRIBUTE_LOGGED_USERS_HOLDER);
-        String userName = (String) httpSessionEvent.getSession()
-                .getAttribute("username");
-        loggedUsers.remove(userName);
-        httpSessionEvent.getSession().setAttribute(Security.ATTRIBUTE_LOGGED_USERS_HOLDER, loggedUsers);
+        String username = (String) httpSessionEvent
+                .getSession()
+                .getAttribute(Security.ATTRIBUTE_USERNAME_HOLDER);
+        loggedUsers.remove(username);
+        log.info("logged users: " + loggedUsers.toString());
+        httpSessionEvent
+                .getSession()
+                .getServletContext()
+                .setAttribute(Security.ATTRIBUTE_LOGGED_USERS_HOLDER, loggedUsers);
     }
 }
