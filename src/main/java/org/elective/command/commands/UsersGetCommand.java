9d51645f5d98dbfa6d6ee3440a68db355fd09c17
Ilia_Rutkovskii <ilya28rut@gmail.com>
2020-11-02 02:46:52 +0200

    working logic added (use userService#getAllUsers)

--- src/main/java/org/elective/command/commands/UsersGetCommand.java
+++ src/main/java/org/elective/command/commands/UsersGetCommand.java
@@ -1,9 +1,12 @@
 package org.elective.command.commands;
 
+import org.apache.log4j.Logger;
 import org.elective.command.Command;
-import org.elective.dbtools.Importer;
 import org.elective.dbtools.exceptions.ORMException;
 import org.elective.entities.User;
+import org.elective.service.UserService;
+import org.elective.service.WebPaths;
+import org.elective.service.pagination.Page;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,13 +14,22 @@ import java.util.List;
 
 public class UsersGetCommand implements Command {
     private static final String PAGE_NAME = "users";
+    private static final Logger log = Logger.getLogger(UsersGetCommand.class);
+
+    private final UserService userService;
+
+    public UsersGetCommand(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws ORMException {
-        // TODO: condition (for pagination)
-        Importer<User> importer = new Importer<>(null);
-        List<User> users = importer.load(User.class, null);
+        Page page = userService.pagination(request);
+        log.info("Page = " + page.getPage());
+        List<User> users = userService.getAllUsers(page);
         request.setAttribute("users", users);
-        return PAGE_NAME;
+        request.setAttribute("page", page.getPage());
+        request.setAttribute("pages", page.getCount());
+        return WebPaths.nameToPath(PAGE_NAME);
     }
 }
